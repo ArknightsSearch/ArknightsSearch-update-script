edf4149c407c4cd41dffca48358ca1cd33386a2f
meeboo3 <meeboo3@qq.com>
2023-06-05 13:14:56 +0800

    update commit.py

--- .github/scripts/commit.py
+++ .github/scripts/commit.py
@@ -13,11 +13,11 @@ utc = datetime.utcnow().replace(tzinfo=timezone.utc)
 now = utc.astimezone(timezone(timedelta(hours=8)))
 time = now.strftime('%y-%m-%d-%H-%M-%S')
 version = calc_version()[:7]
-commit_message = f'\'[UPDATE] Data:{time}-{version}\''
+commit_message = f'[UPDATE] Data:{time}-{version}'
 
 subprocess.run(f'echo "MESSAGE={commit_message}" >> $GITHUB_ENV', shell=True)
 
 subprocess.run('git config --global user.email noreply@arkfans.top', shell=True)
 subprocess.run('git config --global user.name MeeBooBot_v0', shell=True)
 subprocess.run('git add version', shell=True)
-subprocess.run(f'git commit -m {commit_message}', shell=True)
+subprocess.run(f'git commit -m "{commit_message}"', shell=True)
